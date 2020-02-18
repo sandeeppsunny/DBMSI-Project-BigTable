36e1c2fda95641b89500fb0f61ffee87c0d84495
Sandeep Sunny <sandeepsunny@vmware.com>
2020-02-17 22:52:59 -0700

    Added MapUtils class

---
+++ minjava/javaminibase/src/iterator/MapUtils.java
@@ -0,0 +1,25 @@
+package iterator;
+
+import BigT.*;
+import global.AttrType;
+
+/**
+ * Useful methods for processing BigTable maps
+ */
+public class MapUtils {
+    public static int CompareMapWithMap(Map m1, Map m2, int mapfldno) {
+        return 0;
+    }
+
+    public static boolean Equal(Map m1, Map m2) {
+        return false;
+    }
+
+    public static String Value(Map map, int mapfldno) {
+        return "";
+    }
+
+    public static void SetValue(Map value, Map map, int mapfldno, AttrType fldType) {
+
+    }
+}
