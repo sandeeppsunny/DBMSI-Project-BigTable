a70dbfa487647697cda19d9e48e1fecc7ff2db26
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-03-09 15:54:37 -0700

    Added set_op_map function in Map utils

--- minjava/javaminibase/src/iterator/MapUtils.java
+++ minjava/javaminibase/src/iterator/MapUtils.java
@@ -3,6 +3,7 @@ package iterator;
 import BigT.*;
 import global.AttrType;
 import heap.FieldNumberOutOfBoundException;
+import heap.Tuple;
 
 import java.io.IOException;
 
@@ -65,4 +66,132 @@ public class MapUtils {
         return m1.getRowLabel().equals(m2.getRowLabel()) && m1.getColumnLabel().equals(m2.getColumnLabel())
                 && m1.getTimeStamp() == m2.getTimeStamp() && m1.getValue().equals(m2.getValue());
     }
+    /**
+     * set up the Jtuple's attrtype, string size,field number for using join
+     *
+     * @param Jtuple       reference to an actual tuple  - no memory has been malloced
+     * @param res_attrs    attributes type of result tuple
+     * @param in1          array of the attributes of the tuple (ok)
+     * @param len_in1      num of attributes of in1
+     * @param in2          array of the attributes of the tuple (ok)
+     * @param len_in2      num of attributes of in2
+     * @param t1_str_sizes shows the length of the string fields in S
+     * @param t2_str_sizes shows the length of the string fields in R
+     * @param proj_list    shows what input fields go where in the output tuple
+     * @param nOutFlds     number of outer relation fileds
+     * @throws IOException         some I/O fault
+     * @throws TupleUtilsException exception from this class
+     */
+    public static short[] setup_op_map(Map Jmap, AttrType[] res_attrs,
+                                         AttrType in1[], int len_in1, AttrType in2[],
+                                         int len_in2, short t1_str_sizes[],
+                                         short t2_str_sizes[],
+                                         FldSpec proj_list[], int nOutFlds)
+            throws IOException,
+            TupleUtilsException {
+//        short[] sizesT1 = new short[len_in1];
+//        short[] sizesT2 = new short[len_in2];
+        int i, count = 0;
+
+//        for (i = 0; i < len_in1; i++)
+//            if (in1[i].attrType == AttrType.attrString)
+//                sizesT1[i] = t1_str_sizes[count++];
+
+//        for (count = 0, i = 0; i < len_in2; i++)
+//            if (in2[i].attrType == AttrType.attrString)
+//                sizesT2[i] = t2_str_sizes[count++];
+
+        int n_strs = 3;
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer)
+//                res_attrs[i] = new AttrType(in1[proj_list[i].offset - 1].attrType);
+//            else if (proj_list[i].relation.key == RelSpec.innerRel)
+//                res_attrs[i] = new AttrType(in2[proj_list[i].offset - 1].attrType);
+//        }
+
+        // Now construct the res_str_sizes array.
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer && in1[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                n_strs++;
+//            else if (proj_list[i].relation.key == RelSpec.innerRel && in2[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                n_strs++;
+//        }
+
+        short[] res_str_sizes = new short[]{30, 30, 30};
+        count = 0;
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer && in1[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                res_str_sizes[count++] = sizesT1[proj_list[i].offset - 1];
+//            else if (proj_list[i].relation.key == RelSpec.innerRel && in2[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                res_str_sizes[count++] = sizesT2[proj_list[i].offset - 1];
+//        }
+        try {
+            Jmap.setHdr((short) nOutFlds, null, res_str_sizes);
+        } catch (Exception e) {
+            throw new TupleUtilsException(e, "setHdr() failed");
+        }
+        return res_str_sizes;
+    }
+
+
+    /**
+     * set up the Jtuple's attrtype, string size,field number for using project
+     *
+     * @param Jtuple       reference to an actual tuple  - no memory has been malloced
+     * @param res_attrs    attributes type of result tuple
+     * @param in1          array of the attributes of the tuple (ok)
+     * @param len_in1      num of attributes of in1
+     * @param t1_str_sizes shows the length of the string fields in S
+     * @param proj_list    shows what input fields go where in the output tuple
+     * @param nOutFlds     number of outer relation fileds
+     * @throws IOException         some I/O fault
+     * @throws TupleUtilsException exception from this class
+     * @throws InvalidRelation     invalid relation
+     */
+
+    public static short[] setup_op_map(Tuple Jmap, AttrType res_attrs[],
+                                         AttrType in1[], int len_in1,
+                                         short t1_str_sizes[],
+                                         FldSpec proj_list[], int nOutFlds)
+            throws IOException,
+            TupleUtilsException,
+            InvalidRelation {
+//        short[] sizesT1 = new short[len_in1];
+        int i, count = 0;
+
+//        for (i = 0; i < len_in1; i++)
+//            if (in1[i].attrType == AttrType.attrString)
+//                sizesT1[i] = t1_str_sizes[count++];
+
+        int n_strs = 3;
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer)
+//                res_attrs[i] = new AttrType(in1[proj_list[i].offset - 1].attrType);
+//
+//            else throw new InvalidRelation("Invalid relation -innerRel");
+//        }
+
+        // Now construct the res_str_sizes array.
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer
+//                    && in1[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                n_strs++;
+//        }
+
+        short[] res_str_sizes = new short[]{30, 30, 30};
+        count = 0;
+//        for (i = 0; i < nOutFlds; i++) {
+//            if (proj_list[i].relation.key == RelSpec.outer
+//                    && in1[proj_list[i].offset - 1].attrType == AttrType.attrString)
+//                res_str_sizes[count++] = sizesT1[proj_list[i].offset - 1];
+//        }
+
+        try {
+            Jmap.setHdr((short) nOutFlds, null, res_str_sizes);
+        } catch (Exception e) {
+            throw new TupleUtilsException(e, "setHdr() failed");
+        }
+        return res_str_sizes;
+    }
+
 }
