e1d3d79c4095c0e723545ab7dada6807e7859315
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-04-06 23:48:52 -0700

    Implementation & Bug fix: Row join implementation done [Removal of duplicates remaining]

--- minjava/javaminibase/src/BigT/RowJoin.java
+++ minjava/javaminibase/src/BigT/RowJoin.java
@@ -6,11 +6,10 @@ import heap.HFBufMgrException;
 import heap.HFDiskMgrException;
 import heap.HFException;
 import heap.Heapfile;
-import iterator.FileScanMap;
-import iterator.MapUtils;
-import iterator.SortMap;
+import iterator.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class RowJoin{
     private String bigTable1;
@@ -21,8 +20,8 @@ public class RowJoin{
     private Heapfile tempHeapFile2;
     private String heapFileName1;
     private String heapFileName2;
-    private CombinedStream stream1;
-    private CombinedStream stream2;
+    private Stream stream1;
+    private Stream stream2;
     private bigt bigt1;
     private bigt bigt2;
     private int numBuf;
@@ -44,27 +43,27 @@ public class RowJoin{
         try{
             System.out.println("Records in bigt1: " + this.bigt1.getMapCnt());
             System.out.println("Records in bigt2: " + this.bigt2.getMapCnt());
-            this.numBuf = SystemDefs.JavabaseBM.getNumBuffers();
-            this.stream1 = new CombinedStream(this.bigt1, 1, "*",
-                    this.columnFilter, "*", this.numBuf);
-            this.stream2 = new CombinedStream(this.bigt2, 1, "*",
-                    this.columnFilter, "*", this.numBuf);
+            this.numBuf = (int)((SystemDefs.JavabaseBM.getNumBuffers()*3)/8);
         }catch(Exception e){
-            System.err.println("RowJoin.java: Error in getting the combined stream");
+            System.err.println("RowJoin.java: Error in getting the number of records in the bigtables");
             e.printStackTrace();
         }
     }
 
     public void run() throws Exception {
         buildTempHeapFiles();
-        String[] matchingRowlabels =  getMatchingRowLabels();
+        System.out.println("Number of elements in temporary heap file1: " + this.tempHeapFile1.getRecCntMap());
+        System.out.println("Number of elements in temporary heap file1: " + this.tempHeapFile2.getRecCntMap());
+        ArrayList<String[]> matchingRowlabels =  getMatchingRowLabels();
         if(matchingRowlabels == null){
             System.err.println("NO MATCHING ROWS FOUND");
             return;
         }else{
-            System.out.println("Matching Rows: " + matchingRowlabels[0] + " -- " + matchingRowlabels[1]);
+            for(String[] temp: matchingRowlabels){
+                System.out.println("Matching Rows: " + temp[0] + " -- " + temp[1]);
+            }
+            performJoin(matchingRowlabels);
         }
-        createOutputBigTable();
     }
 
     public void buildTempHeapFiles(){
@@ -74,15 +73,24 @@ public class RowJoin{
         }catch(Exception e){
             System.err.println("RowJoin.java: Error caused in creating temporary heapfiles");
         }
+
         try {
             // Creating tempHeapFile 1
+            try{
+                this.stream1 = new Stream(this.bigTable1, null, 1, 1, "*",
+                        this.columnFilter, "*", this.numBuf);
+            }catch(Exception e){
+                System.err.println("RowJoin.java: Error caused in opening stream on bigtable1");
+                return;
+            }
             Map prevMap=null;
-            Map temp = this.stream1.getNext();
+            Map temp = new Map();
+            temp = this.stream1.getNext();
             String rowLabel = "";
             if(temp!=null){
                 temp.setFldOffset(temp.getMapByteArray());
                 rowLabel = temp.getRowLabel();
-                prevMap = temp;
+                prevMap = new Map(temp);
             }
             while (true) {
                 temp = this.stream1.getNext();
@@ -94,22 +102,32 @@ public class RowJoin{
                     this.tempHeapFile1.insertRecordMap(prevMap.getMapByteArray());
                     rowLabel = temp.getRowLabel();
                 }
-                prevMap = temp;
+                prevMap = new Map(temp);
 
             }
             if(prevMap!=null) {
                 this.tempHeapFile1.insertRecordMap(prevMap.getMapByteArray());
             }
+            this.stream1.closestream();
 
             // Creating tempHeapFile 2
-            prevMap=null;
+            try{
+                this.stream2 = new Stream(this.bigTable2, null, 1, 1, "*",
+                        this.columnFilter, "*", this.numBuf);
+            }catch(Exception e){
+                System.err.println("RowJoin.java: Error caused in opening stream on bigtable2");
+                return;
+            }
+
+            prevMap=new Map();
             rowLabel = "";
             temp = this.stream2.getNext();
             if(temp!=null){
                 temp.setFldOffset(temp.getMapByteArray());
                 rowLabel = temp.getRowLabel();
-                prevMap = temp;
+                prevMap = new Map(temp);
             }
+            prevMap.setFldOffset(prevMap.getMapByteArray());
             while (true) {
                 temp = this.stream2.getNext();
                 if (temp == null) {
@@ -120,61 +138,62 @@ public class RowJoin{
                     this.tempHeapFile2.insertRecordMap(prevMap.getMapByteArray());
                     rowLabel = temp.getRowLabel();
                 }
-                prevMap = temp;
+                prevMap = new Map(temp);
 
             }
             if(prevMap!=null) {
                 this.tempHeapFile2.insertRecordMap(prevMap.getMapByteArray());
             }
-            this.stream1.closeCombinedStream();
-            this.stream2.closeCombinedStream();
+            this.stream2.closestream();
 
         }catch(Exception e){
             System.err.println("RowJoin.java: Error caused in building temporary heapfiles");
         }
     }
 
-    public String[] getMatchingRowLabels() throws Exception {
-        String[] res = null;
+    public ArrayList<String[]> getMatchingRowLabels() throws Exception {
+        ArrayList<String[]> res = new ArrayList<>();
+        String[] temp = new String[2];
         SortMap[] sortMaps = getSortedStreams();
         if(sortMaps!=null){
-            Map mapLeft = sortMaps[0].get_next();
-            Map mapRight = sortMaps[1].get_next();
-            if(mapLeft!=null){
-                mapLeft.setFldOffset(mapLeft.getMapByteArray());
-            }
-            if(mapRight!=null){
-                mapRight.setFldOffset(mapRight.getMapByteArray());
-            }
+            Map mapLeft = null;
+            Map mapRight = null;
             while(true){
-                if(mapLeft.getValue().compareTo(mapRight.getValue()) < 0){
-                    mapLeft = sortMaps[0].get_next();
-                }else if(mapLeft.getValue().compareTo(mapRight.getValue()) > 0){
-                    mapRight = sortMaps[1].get_next();
-                }else{
-                    res = new String[2];
-                    res[0] = mapLeft.getValue();
-                    res[1] = mapRight.getValue();
+                if((mapLeft = sortMaps[0].get_next()) == null){
                     break;
                 }
-
-                if(mapLeft == null || mapRight == null){
-                    break;
-                }
-                else{
-                    mapLeft.setFldOffset(mapLeft.getMapByteArray());
+                mapLeft.setFldOffset(mapLeft.getMapByteArray());
+                while(true){
+                    if((mapRight = sortMaps[1].get_next()) == null){
+                        break;
+                    }
                     mapRight.setFldOffset(mapRight.getMapByteArray());
+                    if(mapLeft.getValue().compareTo(mapRight.getValue()) == 0){
+                        temp = new String[2];
+                        temp[0] = mapLeft.getRowLabel();
+                        temp[1] = mapRight.getRowLabel();
+                        res.add(temp);
+                    }
                 }
+                sortMaps[1].close();
+                FileScanMap fileScanMap2 = new FileScanMap(this.heapFileName2, null, null, false);
+                sortMaps[1] = new SortMap(null, null, null, fileScanMap2,
+                        6, new MapOrder(MapOrder.Ascending), null, this.numBuf);
             }
+            sortMaps[0].close();
         }
         return res;
     }
 
+    /**
+     * Used to get the sortMap where sorting is done based on the values.
+     * @return
+     */
     public SortMap[] getSortedStreams(){
         SortMap[] res = null;
         try{
-            FileScanMap fileScanMap1 = new FileScanMap(this.heapFileName1, null, null);
-            FileScanMap fileScanMap2 = new FileScanMap(this.heapFileName2, null, null);
+            FileScanMap fileScanMap1 = new FileScanMap(this.heapFileName1, null, null, false);
+            FileScanMap fileScanMap2 = new FileScanMap(this.heapFileName2, null, null, false);
             res  = new SortMap[2];
             res[0] = new SortMap(null, null, null, fileScanMap1,
                     6, new MapOrder(MapOrder.Ascending), null, this.numBuf);
@@ -187,7 +206,71 @@ public class RowJoin{
         return res;
     }
 
-    public void createOutputBigTable(){
+    public void performJoin(ArrayList<String[]> matchingRowLabels){
+        bigt outBigT;
+        try{
+            outBigT = new bigt(this.outBigTable, true);
+        }catch(Exception e){
+            System.err.println("RowJoin.java: Exception thrown in creating output bigtable");
+            e.printStackTrace();
+            return;
+        }
+        Stream tempStream;
+        try{
+            for(String[] matchingRows: matchingRowLabels){
+                tempStream = new Stream(this.bigTable1, null, 1, 1
+                        , matchingRows[0], "*", "*", this.numBuf);
+                Map tempMap = null;
+                String tempColLabel;
+                while(true){
+                    if((tempMap = tempStream.getNext()) == null){
+                        break;
+                    }
+                    tempMap.setFldOffset(tempMap.getMapByteArray());
+                    tempMap.setRowLabel(matchingRows[0] + ":" + matchingRows[1]);
+                    if(!tempMap.getColumnLabel().equals(this.columnFilter)){
+                        tempColLabel = tempMap.getColumnLabel();
+                        tempMap.setColumnLabel(matchingRows[0] + "_" + tempColLabel);
+                    }
+                    outBigT.insertMap(tempMap, 1);
+                }
+                tempStream.closestream();
+
+                tempStream = new Stream(this.bigTable2, null, 1, 1
+                        , matchingRows[1], "*", "*", this.numBuf);
+
+                tempMap = null;
+                tempColLabel = "";
+                while(true){
+                    if((tempMap = tempStream.getNext()) == null){
+                        break;
+                    }
+                    tempMap.setFldOffset(tempMap.getMapByteArray());
+                    tempMap.setRowLabel(matchingRows[0] + ":" + matchingRows[1]);
+                    if(!tempMap.getColumnLabel().equals(this.columnFilter)){
+                        tempColLabel = tempMap.getColumnLabel();
+                        tempMap.setColumnLabel(matchingRows[1] + "_" + tempColLabel);
+                    }
+                    outBigT.insertMap(tempMap, 1);
+                }
+                tempStream.closestream();
+
+            }
+        }catch(Exception e){
+            System.err.println("RowJoin.java: Exception thrown while inserting records into output bigtable");
+            e.printStackTrace();
+            return;
+        }
+        try{
+            System.out.println("ROWJOIN SUCCESS: NUMBER OF RECORDS IN OUTPUT BIGTABLE: " + outBigT.getMapCnt());
+        }catch(Exception e){
+            System.err.println("RowJoin.java: Exception thrown in retrieving the number of records in output bigtable");
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    public void deleteDuplicateRecords(){
 
     }
 
